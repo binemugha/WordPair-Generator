c7f519902a6ed4a6f07aa1e2f202945c048dd9a9
ben-drohealth <bennyblasco@gmail.com>
2021-06-08 22:02:05 +0100

    Shared folder

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
+import 'random_words.dart';
 
 void main() {
   runApp(MyApp());
@@ -15,50 +15,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(title: 'WordPair Generator'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  final _randomWordsPairs = <WordPair>[];
-  final wordPair = WordPair.random();
-  Widget _buildList() {
-    return ListView.builder(
-      padding: const EdgeInsets.all(16.0),
-      itemBuilder: (context, item) {
-        if (item.isOdd) return Divider();
-
-        final index = item ~/ 2;
-
-        if (index >= _randomWordsPairs.length) {
-          _randomWordsPairs.addAll(generateWordPairs().take(10));
-        }
-        return _buildRow(_randomWordsPairs[index]);
-      },
-    );
-  }
-
-  Widget _buildRow(WordPair pair) {
-    return ListTile(
-        title: Text(pair.asPascalCase, style: TextStyle(fontSize: 18.0)));
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: _buildList(),
     );
   }
 }
